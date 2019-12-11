10f89401d48e5081bf8342fc1a4a7f97b9bce1d5
Andreas Olsson <molnify@gmail.com>
2019-12-11 20:53:34 +0100

    first commit

---
+++ src/main/java/com/example/bookservice/BookController.java
@@ -0,0 +1,39 @@
+package com.example.bookservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    @Autowired
+    BookRepository repository;
+
+    @GetMapping("/book")
+    public List<Book> books() {
+        return (List<Book>)repository.findAll();
+    }
+
+    @GetMapping("/book/{id}")
+    public Book books(@PathVariable Long id) {
+        return repository.findById(id).get();
+    }
+
+    @PostMapping("/book")
+    public Book post(@RequestBody Book book) {
+        return repository.save(book);
+    }
+
+    @PutMapping("/book/{id}")
+    public Book put(@PathVariable Long id, @RequestBody Book book) {
+        return repository.save(book);
+    }
+
+    @DeleteMapping("/book/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+}
